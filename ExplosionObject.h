875e5d3363df6ac9ad310c4d2dc00bed92fc8c09
quanghuynguyen1902 <39359294+quanghuynguyen1902@users.noreply.github.com>
2018-05-17 11:31:19 +0700

    Create ExplosionObject.h

---
+++ ExplosionObject.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "BaseObject.h"
+#include "Common.h"
+
+const int EXP_WIDTH = 165;
+const int EXP_HEIGHT = 165;
+
+class ExplosionObject : public BaseObject
+{
+public:
+	ExplosionObject();
+	~ExplosionObject();
+	void set_clip();
+	void set_frame(const int& fr) { frame_ = fr; }
+	void ShowEx(SDL_Surface* des);
+
+private:
+	int frame_;
+	SDL_Rect clip_[4];
+};
+
+
